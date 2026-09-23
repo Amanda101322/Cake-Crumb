c9857cb06159de0d7f0e9580cc14e833a2521a9c
amanda101322 <edetamanda841@gmail.com>
2026-09-23 16:50:21 +0100

    add close button to recipe modal

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const button = document.querySelector(".btn");
 const recipe = document.querySelector(".full-recipe");
+const modal = document.querySelector(".recipe-modal");
+const closeButton = document.querySelector(".close-recipe");
+
+closeButton.addEventListener("click", () => {
+        modal.classList.add("hidden");
+        button.textContent = "View Recipe";
+});
 button.addEventListener("click", () => {
-recipe.classList.toggle("hidden");
-if (recipe.classList.contains("hidden")) {
+modal.classList.toggle("hidden");
+
+if (modal.classList.contains("hidden")) {
     button.textContent = "View Recipe";
 } else {
     button.textContent = "Hide Recipe";
     }
-});
+});     
