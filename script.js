175f0debef453b8f233db5b7fd465ada9f86e5fe
amanda101322 <edetamanda841@gmail.com>
2026-09-24 15:11:29 +0100

    add recipe modals and modal interactions to all cake cards

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-const button = document.querySelector(".btn");
-const recipe = document.querySelector(".full-recipe");
-const modal = document.querySelector(".recipe-modal");
-const closeButton = document.querySelector(".close-recipe");
+const recipeCards = document.querySelectorAll("#recipes article");
+recipeCards.forEach((card) => {
+  const button = card.querySelector(".btn");
+  const modal = card.querySelector(".recipe-modal");
+  if (!modal) return;
+  const closeButton = modal.querySelector(".close-recipe");
 
-closeButton.addEventListener("click", () => {
-        modal.classList.add("hidden");
-        button.textContent = "View Recipe";
+  closeButton.addEventListener("click", () => {
+    modal.classList.add("hidden");
+  });
+  button.addEventListener("click", () => {
+    modal.classList.remove("hidden");
+  });
 });
-button.addEventListener("click", () => {
-modal.classList.toggle("hidden");
-
-if (modal.classList.contains("hidden")) {
-    button.textContent = "View Recipe";
-} else {
-    button.textContent = "Hide Recipe";
-    }
-});     
+     
